861ccac15c62d22d808ae350e70e8bdf27764743
Igal Emona <igal.emona@gmail.com>
2026-04-18 13:25:54 +0300

    refactor: remove temporary scratch scripts and diagnostic utilities

--- brain/05b94e32-673c-46f7-a1fa-3fa65cbe61ce/scratch/list_tables.py
+++
@@ -1,6 +0,0 @@
-from pipeline.db import db
-
-query = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"
-tables = db.fetch_all(query)
-for table in tables:
-    print(table['table_name'])
